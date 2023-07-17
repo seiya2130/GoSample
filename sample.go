b697981c6d3b2787443d036967d9254873722d21
Seiya_Satoh <satohseiya0312@gmail.com>
2023-07-17 10:30:53 +0900

    出力系追加

--- sample.go
+++ sample.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello World!");
+	fmt.Println("Hello World!");
+	num := 123
+	str := "abc"
+	fmt.Print(num, str)
+	fmt.Println("=====")
+	fmt.Println(num, str)
+	fmt.Println("=====")
+	fmt.Printf("num=%d, str=%s", num, str)
 }
